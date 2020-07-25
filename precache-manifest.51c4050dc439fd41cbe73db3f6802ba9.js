c44538fdf084fccd5f413e8032fcf2bb381aa88e
chauhandharmik <68230982+chauhandharmik@users.noreply.github.com>
2020-07-25 10:48:45 +0530

    Updates

---
+++ precache-manifest.51c4050dc439fd41cbe73db3f6802ba9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f0ffe2f8cb7fd15af7833be312d5ed",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "e3d644309432e47b2a81",
+    "url": "/portfolio-react/static/css/main.3e7887ec.chunk.css"
+  },
+  {
+    "revision": "bee4176455ac508e5196",
+    "url": "/portfolio-react/static/js/2.3ae98b0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-react/static/js/2.3ae98b0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3d644309432e47b2a81",
+    "url": "/portfolio-react/static/js/main.d259643a.chunk.js"
+  },
+  {
+    "revision": "ae85df2f49c0b16271ce",
+    "url": "/portfolio-react/static/js/runtime-main.d8848083.js"
+  },
+  {
+    "revision": "eb2ed28ab7b4d5fb653164394796a227",
+    "url": "/portfolio-react/static/media/f1.eb2ed28a.jpg"
+  },
+  {
+    "revision": "24c4e7b4c79e6011aff61e9b3ec59d37",
+    "url": "/portfolio-react/static/media/p2.24c4e7b4.png"
+  },
+  {
+    "revision": "1f86dd2bbd6375713ce89ecc41823e4b",
+    "url": "/portfolio-react/static/media/shopping.1f86dd2b.jpg"
+  },
+  {
+    "revision": "c02ae8b401147c34549c6c98c7289eb8",
+    "url": "/portfolio-react/static/media/webproject.c02ae8b4.jpg"
+  }
+]);
